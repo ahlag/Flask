1f1821194ef5d853c503314117d3f4738dc43fd8
Chang Yong Lik <theeahlag@gmail.com>
2020-06-09 14:00:23 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Project Title
+
+One Paragraph of project description goes here
+
---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, request
+import re
+import numexpr
+app = Flask(__name__)
+
+def validate_url(url):
+    regex = re.compile(
+            r'^(?:http|ftp)s?://' # http:// or https://
+            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+            r'localhost|' #localhost...
+            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+            r'(?::\d+)?' # optional port
+            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+    return re.match(regex, url) is not None
+
+def get_query_parameter(url):
+    return url.split("calc?",1)[1]
+
+@app.route('/')
+def Homepage():
+    return "Homepage"
+
+@app.route("/calc")
+def calc():
+
+
+    url = request.url
+
+    if validate_url(url) is False:
+        return "ERROR"
+
+    try:
+        return str(numexpr.evaluate(get_query_parameter(url)))
+    except KeyError:
+        return "ERROR"
+
+    print(request.url)
+    print(type(request.url))
+
+    return url
+
+    #
+    #
+    #
+    #
+    # return request.url
+
+if __name__ == '__main__':
+    app.run()
