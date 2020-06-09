daa97585dea0f6deb818c1f6ba642be97d6a5e14
Chang Yong Lik <theeahlag@gmail.com>
2020-06-09 20:19:26 +0900

    Removed url validation

--- app.py
+++ app.py
@@ -3,17 +3,6 @@ import re
 import numexpr
 app = Flask(__name__)
 
-def validate_url(url):
-    regex = re.compile(
-            r'^(?:http|ftp)s?://' # http:// or https://
-            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
-            r'localhost|' #localhost...
-            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
-            r'(?::\d+)?' # optional port
-            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-
-    return re.match(regex, url) is not None
-
 def get_query_parameter(url):
     return url.split("calc?",1)[1]
 
@@ -24,11 +13,7 @@ def Homepage():
 @app.route("/calc")
 def calc():
 
-
     url = request.url
-
-    if validate_url(url) is False:
-        return "ERROR"
 
     try:
         return str(numexpr.evaluate(get_query_parameter(url)))
